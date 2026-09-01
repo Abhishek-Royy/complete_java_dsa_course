a0b8be86c39a79f46085c9e1e43900c56d603912
Abhishek-Royy <abhishek699.roy@gmail.com>
2026-09-01 21:51:25 +0530

    feat: implement stack data structure using Stack class

---
+++ stackUsingCollectionFramwork.java
@@ -0,0 +1,20 @@
+
+import java.util.Stack;
+
+
+public class stackUsingCollectionFramwork {
+
+    public static void main(String[] args) {
+
+        Stack <Integer> s=new Stack<>();
+
+        s.push(40);
+        s.push(10);
+        s.push(20);
+       System.out.println(s);
+       System.out.println(s.pop());
+System.out.println(s);
+       System.out.println(s.peek());
+
+    }
+}
